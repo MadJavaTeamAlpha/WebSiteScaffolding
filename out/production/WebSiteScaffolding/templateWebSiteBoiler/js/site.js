3b97b18c8c32cdb1f0a88d8e635da5b68aa0c2e9
dhafenstein <dhafenstein@madisoncollege.edu>
2015-11-17 15:25:00 -0600

    finished more logic; files are being copied into the new directory successfully

---
+++ out/production/WebSiteScaffolding/templateWebSiteBoiler/js/site.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+    
+    
+    
+   //  Init validate $("#commentForm").validate();
+    
+});
--- src/alphaWebService/FileCreator.java
+++ src/alphaWebService/FileCreator.java
@@ -1,120 +1,164 @@
 package alphaWebService;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+import org.apache.commons.io.*;
 
 /**
  * Created by Drakenstein on 11/16/2015.
  */
 public class FileCreator {
 
-    String line = "";
-    Path inputFile = Paths.get("C:\\Users\\Drakenstein\\Documents\\GitHub\\myInputFile.txt");
-    Path outputFile = Paths.get("C:\\Users\\Drakenstein\\Documents\\GitHub\\myOutputFile.txt");
-    Charset charset = StandardCharsets.UTF_8;
+//    String line = "";
+//    Charset charset = StandardCharsets.UTF_8;
 
-    String cssparam;
-    String frameworkparam;
-    String scriptparam;
+    //request params coming in from the URL query string; set here for development
+    String cssparam = "Y"; //"Y" or "N" or ""
+    String frameworkparam = "foundation"; //"bootstrap" or "foundation"
+    String scriptparam = "jForm"; //"js" or "jQuery" or "jForm"
 
-    String styleFile = "Style.css";
-    String bootstrapFile = "Bootstrap.css";
-    String foundationFile = "Foundation.css";
-    String jsFile = "Site.js";
-    String jQueryFile = "Jquery.js";
-    String jQueryValidateFile = "Jquery.validate.js";
+    //make paths tbd
+    //Path indexHTMLFilePath = Paths.get("./src/resources/index.html");
+    //Path productFolderPath = Paths.get("./src/templateWebSiteProduct");
+    // Path productIndexFilePath = Paths.get(productFolder + "/index.html");
 
+    //make files
+    //---template boiler plate files
+    String templateWebSiteBoilerString = "./src/resources/templateWebSiteBoiler";
+    File indexHTMLFile = new File(templateWebSiteBoilerString + "/index.html");
+    File cssFileFolder = new File(templateWebSiteBoilerString + "/css");
+    File bootstrapFileFolder = new File(templateWebSiteBoilerString + "/assets/bootstrap");
+    File foundationFileFolder = new File(templateWebSiteBoilerString + "/assets/foundation");
+    File jsFileFolder = new File(templateWebSiteBoilerString + "/js");
+    File jQueryFile = new File(templateWebSiteBoilerString + "/assets/jquery.js");
+    File jQueryValidationFileFolder = new File(templateWebSiteBoilerString + "/assets/jquery-validation");
+
+    //---product files
+    String productFolderString = "./src/templateWebSiteProduct";
+    File productFileFolder = new File("./src/templateWebSiteProduct");
+    File productAssetFileFolder = new File(productFolderString + "/assets");
+
+
+
+    public static void main(String[] args)
+    {
+        FileCreator app = new FileCreator();
+        app.writeIndexHTML();
+        app.interrogateParams();
+    }
+
+
+    //write a root file for the web site files
+    public void writeIndexHTML()
+    {
+        makeDir(productFileFolder);
+        copyFileToDir(indexHTMLFile, productFileFolder);
+    }
 
     public void interrogateParams()
     {
-
-        if (!cssparam.isEmpty())
+        if (!cssparam.isEmpty() && cssparam.equals("Y"))
         {
-            if (cssparam.equals("Y"))
-            {
-                readFile();
-                //add site.css to style que
-                //create blank style.css file
-            }
+            copyDirToDir(cssFileFolder,productFileFolder);
+//TODO
+//          add to script queue
         } //end cssparam
+
 
         if (!frameworkparam.isEmpty())
         {
-                String key = frameworkparam; //"bootstrap" or "foundation"
-                boolean boolBootstrap = false;
-                boolean boolFoundation = false;
+            String key = frameworkparam; //"bootstrap" or "foundation"
+            boolean boolBootstrap = false;
+            boolean boolFoundation = false;
 
-                switch (key) {
-                    case "bootstrap":
-                        boolBootstrap = true;
-                        break;
-                    case "foundation":
-                        boolFoundation = true;
-                        break;
-                }
+            switch (key)
+            {
+                case "bootstrap":
+                    boolBootstrap = true;
+                    break;
+                case "foundation":
+                    boolFoundation = true;
+                    break;
+            }
 
-                if (boolBootstrap) {
-//                add bootstrap.js to script que
-//                add bootstrap.css to style que
-//                add bootstrap directory to assets directory
+            //make the assets directory
+            makeDir(productAssetFileFolder);
 
-                }
-                else if (boolFoundation) {
-//                add bootstrap.js to script que
-//                add bootstrap.css to style que
-//                add bootstrap directory to assets directory
-                }
+            if (boolBootstrap)
+            {
+                //FYI in IntelliJ Project Window it might look like this: assets.bootstrap (= assets/bootstrap/)
+                copyDirToDir(bootstrapFileFolder,productAssetFileFolder);
+//TODO
+//                add bootstrap.js to script queue
+//                add bootstrap.css to style queue
+
+            }
+            else if (boolFoundation)
+            {
+                copyDirToDir(foundationFileFolder,productAssetFileFolder);
+//TODO
+//                add bootstrap.js to script queue
+//                add bootstrap.css to style queue
+            }
 
         } //end frameworkparam
 
         if (!scriptparam.isEmpty())
         {
-            if (scriptparam.equals("Y"))
+            String key = scriptparam; //"js" or "jQuery" or "jForm"
+            boolean boolJS = false;
+            boolean boolJQuery = false;
+            boolean boolJForm = false;
+
+            switch (key)
             {
-                String key = scriptparam; //"js" or "jQuery" or "jForm"
-                boolean boolJS = false;
-                boolean boolJQuery = false;
-                boolean boolJForm = false;
+                case "js":
+                    boolJS = true;
+                    break;
+                case "jQuery":
+                    boolJQuery = true;
+                    break;
+                case "jForm":
+                    boolJForm = true;
+                    break;
+            }
 
-                switch (key) {
-                    case "js":
-                        boolJS = true;
-                        break;
-                    case "jQuery":
-                        boolJQuery = true;
-                        break;
-                    case "jForm":
-                        boolJForm = true;
-                        break;
-                }
+            if (boolJS)
+            {
+                copyDirToDir(jsFileFolder,productFileFolder);
 
-                if (boolJS) {
+            }
+            else if (boolJQuery)
+            {
+                //add jQuery.js to assets directory
+                copyFileToDir(jQueryFile, productAssetFileFolder);
 
-//                add blank site.js to js directory
-
-                }
-                else if (boolJQuery) {
-
-//                add jQuery.js to assets directory
-//                add site.js to js directory
+                //add site.js to js directory
+                copyDirToDir(jsFileFolder,productFileFolder);
+//TODO
 //                write onReady open to site.js
 //                write onReady close to site.js
-                }
-                else if (boolJForm) {
-//                add jQuery.validate to assets directory
-//                add jQuery.js to assets directory
-//                add site.js to js directory
+
+            }
+            else if (boolJForm)
+            {
+                //add jQuery.js to assets directory
+                copyFileToDir(jQueryFile, productAssetFileFolder);
+
+                //add site.js to js directory
+                copyDirToDir(jsFileFolder,productFileFolder);
+
+                //add jQuery.validate to assets directory
+                copyDirToDir(jQueryValidationFileFolder,productAssetFileFolder);
+//TODO
 //                write onReady open to site.js
 //                write jQuery.validate instantiation to site.js
 //                write onReady close to site.js
 
-                }
             }
         } //end scriptparam
 
@@ -141,10 +185,10 @@ write close body
 Write close html
 
 
-*/
-    }
 
-    public void readFile()
+
+
+    public void readFileOrFolderAndWriteToTemplateFolder()
     {
 
         try (BufferedReader reader = Files.newBufferedReader(inputFile, charset))
@@ -167,14 +211,78 @@ Write close html
             System.err.println("Unable to read the file: fileName");
         }
     }
+    */
 
 
+    } //end interrogateParams()
 
+    public void copyFileToDir(File srcFile, File destDir)
+    {
+        try
+        {
+            FileUtils.copyFileToDirectory(srcFile, destDir);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
+    public void copyDirToDir(File srcDir, File destDir)
+    {
+        try
+        {
+            FileUtils.copyDirectoryToDirectory(srcDir, destDir);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
+    public void makeDir(File dir)
+    {
+        try
+        {
+            FileUtils.forceMkdir(dir);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
+    public void deleteDir(File dir)
+    {
+        try
+        {
+            FileUtils.deleteDirectory(dir);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-
+//TODO
+    //configure index.html
+    //zip up folder
+    //send response of zip file
+    //delete the files in project after sending the response
+ /*   try {
+        Thread.sleep(10000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    deleteDir(productFileFolder); */
 
 
 
